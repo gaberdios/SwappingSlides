33c7d0cb85f632c218ee790b9a3a26fe92b08c46
gaberdios <gaberdios@gmail.com>
2016-08-22 23:12:47 +0200

    final countdown

--- SwappingSlides/PlaySongVC.swift
+++ SwappingSlides/PlaySongVC.swift
@@ -35,7 +35,7 @@ class PlaySongVC: UIViewController {
     
 
     /*
-    // MARK: - Navigation
+    // MARK: - Navigation 
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
